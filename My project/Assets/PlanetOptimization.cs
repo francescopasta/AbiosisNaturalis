6e78342fd6ef2ec0d755ffe9782a9520f97cbb11
francescopasta <pastore.francesco01@gmail.com>
2025-07-02 19:51:04 +0200

    Shader Fix 2.0

--- My project/Assets/PlanetOptimization.cs
+++ My project/Assets/PlanetOptimization.cs
@@ -4,42 +4,71 @@ public class PlanetOptimization : MonoBehaviour
 {
     public Transform planet;
 
-    public Vector2 fireAreaEnabledRange;
-    public Vector2 waterAreaEnabledRange;
-    public Vector2 crystalAreaEnabledRange;
+    public Vector2 fireAreaEnabledRange;     // e.g., (25, 125)
+    public Vector2 waterAreaEnabledRange;    // e.g., (335, 25) for -25 to 25
+    public Vector2 crystalAreaEnabledRange;  // e.g., (235, 335) for -125 to -25
 
     public GameObject firePlain;
     public GameObject waterPlain;
     public GameObject crystalPlain;
 
+    private bool waterEnabled = false;
+    private bool fireEnabled = false;
+    private bool crystalEnabled = false;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        if (planet.transform.rotation.z >= waterAreaEnabledRange[1] && planet.transform.rotation.z <= waterAreaEnabledRange[2])
-        {
-            firePlain.SetActive(false);
-            crystalPlain.SetActive(false);
-            waterPlain.SetActive(true);
-        }
-        else if (planet.transform.rotation.z >= fireAreaEnabledRange[1] && planet.transform.rotation.z <= fireAreaEnabledRange[2])
-        {
-            firePlain.SetActive(true);
-            crystalPlain.SetActive(false);
-            waterPlain.SetActive(false);
-        }
-        else
-        {
-            firePlain.SetActive(false);
-            crystalPlain.SetActive(true);
-            waterPlain.SetActive(false);
-        }
+        float zRotation = planet.transform.eulerAngles.z;
 
+        if (IsAngleInRange(zRotation, fireAreaEnabledRange.x, fireAreaEnabledRange.y) && !fireEnabled)
+        {
+            Debug.Log("Enabling fire");
+            firePlain.SetActive(true);
+            waterPlain.SetActive(false);
+            crystalPlain.SetActive(false);
+            fireEnabled = true;
+            waterEnabled = false;
+            crystalEnabled = false;
+        }
+        else if (IsAngleInRange(zRotation, waterAreaEnabledRange.x, waterAreaEnabledRange.y) && !waterEnabled)
+        {
+            Debug.Log("Enabling water");
+            firePlain.SetActive(false);
+            waterPlain.SetActive(true);
+            crystalPlain.SetActive(false);
+            fireEnabled = false;
+            waterEnabled = true;
+            crystalEnabled = false;
+        }
+        else if (IsAngleInRange(zRotation, crystalAreaEnabledRange.x, crystalAreaEnabledRange.y) && !crystalEnabled)
+        {
+            Debug.Log("Enabling crystal");
+            firePlain.SetActive(false);
+            waterPlain.SetActive(false);
+            crystalPlain.SetActive(true);
+            fireEnabled = false;
+            waterEnabled = false;
+            crystalEnabled = true;
+        }
+    }
+
+    // Helper to check if angle is within a circular range
+    bool IsAngleInRange(float angle, float min, float max)
+    {
+        angle = NormalizeAngle(angle);
+        min = NormalizeAngle(min);
+        max = NormalizeAngle(max);
+
+        if (min < max)
+            return angle >= min && angle <= max;
+        else
+            return angle >= min || angle <= max;
+    }
+
+    float NormalizeAngle(float angle)
+    {
+        angle %= 360f;
+        if (angle < 0) angle += 360f;
+        return angle;
     }
 }
